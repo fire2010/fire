b5e5662df00224040e17e72d795d9fb34f4362d4
fire2010 <415196086fire@sian.com>
2017-05-23 19:56:51 +0800

    fire

--- fire-content/fire-content-service/src/test/java/com/fire/jedis/JedisTest.java
+++ fire-content/fire-content-service/src/test/java/com/fire/jedis/JedisTest.java
@@ -1,8 +1,13 @@
 package com.fire.jedis;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Test;
 
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPool;
 
 /**
@@ -41,5 +46,25 @@ public class JedisTest {
 		jedis.close();
 		//系统关闭前关闭连接池
 		jedisPool.close();
+	}
+	
+	@Test
+	public void testJedisCluster(){
+		//创建一个JedisCluster操redis,构造参数set类型，集合中每个元素是HostAndPort类型
+		Set<HostAndPort> nodes = new HashSet<>();
+		//向集合中添加节点
+		nodes.add(new HostAndPort("192.168.249.132", 6380));
+		nodes.add(new HostAndPort("192.168.249.132", 6381));
+		nodes.add(new HostAndPort("192.168.249.132", 6382));
+		nodes.add(new HostAndPort("192.168.249.132", 6383));
+		nodes.add(new HostAndPort("192.168.249.132", 6384));
+		nodes.add(new HostAndPort("192.168.249.132", 6385));
+		JedisCluster jedisCluster = new JedisCluster(nodes );
+		//直接使用jedisCluster操作redis,自带连接池。jedisCluster对象可以是单例的
+		jedisCluster.set("cluster-test", "123456789");
+		String value = jedisCluster.get("cluster-test");
+		System.out.println(value);
+		//系统关闭前关闭jedisCluster
+		jedisCluster.close();
 	}
 }
